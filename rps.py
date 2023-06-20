be3cde89b7d1bc328a64c1e1dcc457dda1ebc13b
unknown <vijaylogin91@gmail.com>
2023-06-20 15:47:26 +0530

    new

---
+++ rps.py
@@ -0,0 +1,10 @@
+user = input("enter any no.(4,5,6)")
+move = (4,5,6)
+import random
+cpu = random.move
+while True :
+    if user == 4 and cpu ==4:
+        print("its tie")
+    elif user ==4 & cpu ==5:
+         print("user win")
+    
