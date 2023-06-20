3ab9ca3a1893b245523e9f2cbd17773f25ef64ad
unknown <vijaylogin91@gmail.com>
2023-06-20 14:15:37 +0530

    fbivkbfiv

---
+++ Sure! Here is a code in python to add tw.py
@@ -0,0 +1,19 @@
+Sure! Here is a code in python to add two numbers:
+
+```python
+# storing two numbers in variables
+num1 = 5
+num2 = 7
+
+# adding two numbers
+sum = num1 + num2
+
+# printing the sum of two numbers
+print("The sum of", num1, "and", num2, "is", sum)
+```
+
+Explanation:
+
+1. We first store the two numbers that we want to add in variables `num1` and `num2`.
+2. We then add `num1` and `num2` and store the result in variable `sum`.
+3. Finally, we print the result using the `print()` function by combining strings and variables with the '+' operator.
---
+++ add.py
@@ -0,0 +1,5 @@
+
+num1 = 120
+num2 = 20
+result = num1 + num2
+print(result)
